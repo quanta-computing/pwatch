1a49c21b50fa6945bada5bbf5df4f9e959c9cb98
Matthieu Rosinski <korrigan@slash16.org>
2015-02-05 15:19:08 +0100

    added setup.py and friends

---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+Setuptools script for pwatch
+
+"""
+from setuptools import setup
+
+def readme():
+    """
+    Extracts readme contents
+
+    """
+    with open('README.md') as f:
+        return f.read()
+
+def requirements():
+    """
+    Extacts requirements.txt contents
+
+    """
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
+setup(
+    name='pwatch',
+    version='1.0.0',
+    description='Simple tool to watch and report processes with excessive resource usage',
+    long_description=readme(),
+    license='MIT',
+    url='https://github.com/quanta-computing/pwatch',
+    author="Matthieu 'Korrigan' Rosinski",
+    author_email='mro@quanta-computing.com',
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: System :: Monitoring',
+        'Topic :: Utilities',
+    ],
+    packages=['pwatch'],
+    entry_points={
+        'console_scripts': [
+            'pwatch = pwatch.__main__:main',
+        ],
+    },
+    install_requires=requirements(),
+)
